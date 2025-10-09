6cdb626d294cb1d9bfe06ef5a3cdfb1b3ab0e9a8
KaungPyae223 <kaungpyaeaung8123@gmail.com>
2025-10-09 13:27:12 +0630

    feat: add authentication

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+
+        <p class="bg-black">Hello</p>
+    </body>
+</html>
