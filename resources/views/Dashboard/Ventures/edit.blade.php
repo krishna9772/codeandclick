c3a316cb79019d13654d80f20b0b8abfe7bc83de
KaungPyae223 <kaungpyaeaung8123@gmail.com>
2025-10-13 16:50:22 +0630

    fix: venture update

--- resources/views/Dashboard/Ventures/edit.blade.php
+++ resources/views/Dashboard/Ventures/edit.blade.php
@@ -11,21 +11,22 @@
     <div class="py-12">
         <div class="max-w-7xl space-y-6 mx-auto sm:px-6 lg:px-8">
             <div style="width: 800px;" class="bg-white mx-auto p-4 overflow-hidden shadow-sm sm:rounded-lg">
-              <form action="{{ route('bloglist.store') }}" method="POST" enctype="multipart/form-data">
+              <form action="{{ route('ventures.update',$venture->id) }}" method="POST" enctype="multipart/form-data">
                         @csrf
+                        @method("PUT")
                         <!-- Image Upload with Preview -->
                         <div class="mb-6">
                              <label for="image" class="block text-gray-700 font-bold mb-2">Image</label>
                       
                             <!-- Image Preview Container -->
-                            <div id="imagePreviewContainer" class="hidden mb-4">
+                            <div id="imagePreviewContainer" class="mb-4">
                                 <img id="imagePreview" src="{{ asset($venture->getFirstMediaUrl('ventures')) }}" alt="Image Preview" class=" w-full object-cover rounded-lg border border-gray-200">
                             </div>
                             
                             <div class="mt-1 flex items-center">
                                 <label for="image" class="cursor-pointer bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                                     <span>Choose an image</span>
-                                    <input id="image"  name="image" type="file" class="sr-only" accept="image/*" onchange="previewImage(this)" required>
+                                    <input id="image"  name="image" type="file" class="sr-only" accept="image/*" onchange="previewImage(this)">
                                 </label>
                                 <span id="fileName" class="ml-4 text-sm text-gray-600">No file chosen</span>
                             </div>
