27c9d3543adfbde80f1ed71be68e56fe1319c2b8
Krishna Aryal <aryalkrishna642@gmail.com>
2026-03-17 19:08:41 +0700

    set default values to seo

--- app/Http/Controllers/SeoController.php
+++ app/Http/Controllers/SeoController.php
@@ -10,6 +10,19 @@ class SeoController extends Controller
     public function index()
     {
         $seo = Seo::where('seoable_type', 'App\Models\Home')->first();
+        $seo = Seo::where('seoable_type', 'App\Models\Page')
+          ->where('seoable_id', 1)
+          ->first();
+
+        SEOTools::setTitle($seo?->title ?? 'Code and Click');
+        SEOTools::setDescription($seo?->description ?? 'Code and Click is a professional software development company providing web development, mobile apps, UI/UX design, and digital solutions.');
+        SEOTools::setCanonical(config('app.url'));
+
+        SEOTools::opengraph()->setUrl(config('app.url') . $link);
+        SEOTools::opengraph()->setTitle($seo?->title ?? 'Code and Click');
+        SEOTools::opengraph()->setDescription($seo?->description ?? 'Code and Click is a professional software development company providing web development, mobile apps, UI/UX design, and digital solutions. ');
+
+        SEOTools::twitter()->setTitle($seo?->title ?? 'Code and Click');
         return view('Dashboard.Seo.index', compact('seo'));
     }
 
