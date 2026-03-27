e9b40242337b737d3570083acc0d8284360a39ed
Krishna Aryal <aryalkrishna642@gmail.com>
2026-03-27 14:51:05 +0630

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+      <link rel="icon" type="image/png" href="{{ asset('images/new-favicon.png') }}" />
+
 
     <!-- Scripts -->
     @vite(['resources/css/admin.css', 'resources/js/app.js'])
